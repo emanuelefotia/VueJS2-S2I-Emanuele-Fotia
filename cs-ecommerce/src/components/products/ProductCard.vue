563bc69cdaefbfb88ed89dc9498429271bb36395
Emanuele <emanuelefotia2@gmail.com>
2022-07-21 19:11:00 +0200

    styles

--- cs-ecommerce/src/components/products/ProductCard.vue
+++ cs-ecommerce/src/components/products/ProductCard.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="card mb-4 shadow-sm">
-    <img :src="product.image" class="card-img-top product-image" />
+  <div class="card">
+    <img :src="product.image" class="card-img-top" />
     <div class="card-body">
-      <h5 class="card-title text-left">{{ product.name }}</h5>
-      <div class="row">
-        <router-link
-          type="button"
-          class="btn btn-primary btn-lg"
-          :to="'/details/' + product.name"
-        >Details</router-link>
-      </div>
+      <router-link :to="'/details/' + product.name">
+        <h5 class="card-title text-left">{{ product.name }}</h5>
+      </router-link>
+      <p class="card-text text-left"><strong>{{ product.price }}€ </strong></p>
+
     </div>
   </div>
 </template>
 <script>
 export default {
-    name: "ProductCard",
-    props: ["product"]
+  name: "ProductCard",
+  props: ["product"]
 };
 </script>
 <style>
-.card .product-image {
-  height: 300px;
+a{
+  color: black;
 }
 </style>
