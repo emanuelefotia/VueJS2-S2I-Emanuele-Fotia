0398458c5f2ddd26a4c60a3b66266b6809239c5e
Emanuele <emanuelefotia2@gmail.com>
2022-07-22 14:39:20 +0200

    styles

--- vuejs-ecommerce/src/components/Nav.vue
+++ vuejs-ecommerce/src/components/Nav.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <nav id="store" class="navbar navbar-expand-lg navbar-dark fixed-top">
-      <!-- Home -->
       <router-link :to="{ name: 'Home' }" class="navbar-brand" style="padding-left: 1rem;">
         Store
       </router-link>
 
-      <div class="ml-auto">
-        <!-- Cart -->
+      <div>
         <router-link :to="{ name: 'Cart' }" class="btn btn-primary my-2 my-sm-0">
           <img src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png" width="50" alt />
           <span class="badge badge-danger badge-pill">{{ cartItemCount }}</span>
